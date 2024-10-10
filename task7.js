2703de8aa6b6327410509f19b4bf09789a47eb5f
Sabnam <sabnamchoudhary10@gmail.com>
2024-10-10 10:53:58 +0530

    pushing js problem

--- task7.js
+++ task7.js
@@ -1,4 +1,8 @@
-let array = [1,2,3,4,5,6];
- array.unshift(0);
-
- console.log(array);
+function insertArray ( array , variable){
+    let newArray = [variable];
+    for(let i = 0; i<array.length; i++){
+        newArray.push(array[i])
+    }
+    return newArray
+}
+console.log(insertArray([1,2,3,4],5));
